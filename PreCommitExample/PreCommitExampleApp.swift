6373226c82fb1bf91b193c0250b15342c4fac8fe
Hamza Khan <hamzakhancs15@gmail.com>
2023-07-06 11:02:58 +0200

    Initial Commit

---
+++ PreCommitExample/PreCommitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreCommitExampleApp.swift
+//  PreCommitExample
+//
+//  Created by Hamza Khan on 06.07.23.
+//
+
+import SwiftUI
+
+@main
+struct PreCommitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
